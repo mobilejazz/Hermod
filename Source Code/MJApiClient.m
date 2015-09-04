50565a0879e5dfba87c940ab62d97f4b3f1ce1a4
Joan Martin <vilanovi@gmail.com>
2015-09-04 15:43:04 +0200

    Adding allow fragmetns options on json response reading options.

--- Source Code/MJApiClient.m
+++ Source Code/MJApiClient.m
@@ -46,6 +46,9 @@
         _jsonRequestSerializer = [[AFJSONRequestSerializer alloc] init];
         _jsonResponseSerializer = [[MJJSONResponseSerializer alloc] init];
         
+        // Allowing fragments on json responses.
+        _jsonResponseSerializer.readingOptions = NSJSONReadingAllowFragments;
+        
         // Setting the backend return language
         NSString *language = [[NSLocale preferredLanguages] firstObject];
         [_jsonRequestSerializer setValue:language forHTTPHeaderField:@"Accept-Language"];
