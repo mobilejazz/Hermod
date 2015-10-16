bc1d94f0657feb2faae60536fcc684c46599a82b
Paolo Tagliani <pablosproject@gmail.com>
2015-10-16 22:16:43 +0200

    Updated view controller example

--- Sample Project/ApiClient/ViewController.m
+++ Sample Project/ApiClient/ViewController.m
@@ -23,17 +23,23 @@
 {
     [super viewDidLoad];
     
-    self.apiClient = [[MJApiClient alloc] initWithHost:@"http://www.mocky.io/v2/"];//Testing using Mocki services
+    self.apiClient = [[MJApiClient alloc] initWithConfigurator:^(MJAPiClientConfigurator *configurator) {
+        configurator.apiPath = @"/v2";
+        configurator.host = @"http://www.mocky.io";//Testing using Mocki services
+        configurator.cacheManagement = MJApiClientCacheManagementOffline;
+    }];
+    
+    self.apiClient.logLevel = MJApiClientLogLevelRequests;
 }
 
 - (IBAction)getFakeRequest:(id)sender {
     
-    MJApiRequest *request = [[MJApiRequest alloc] init];
+    MJApiRequest *request = [MJApiRequest requestWithPath:@"562158b5120000714c0113ff"];
     
     //NOTE: I am using Mocky.io to create a fake request with the custom header
     //          Cache-Control : max-age=30
     
-    [_apiClient performRequest:request apiPath:@"562150ae120000704a0113fe" completionBlock:^(MJApiResponse *response, NSInteger key) {
+    [_apiClient performRequest:request completionBlock:^(MJApiResponse *response, NSInteger key) {
         NSString *newString;
         if (response.error) {
             newString = [NSString stringWithFormat:@"Received error:\n%@\n", [response.error localizedDescription]];
