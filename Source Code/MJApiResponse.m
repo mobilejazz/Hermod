878dfc71f4618ff09d254b0dad3f9907689854ff
Joan Martin <vilanovi@gmail.com>
2016-03-08 09:44:54 +0100

    Adding optional language parameter

--- Source Code/MJApiResponse.m
+++ Source Code/MJApiResponse.m
@@ -20,28 +20,18 @@
 
 - (id)init
 {
-    return [self initWithRequest:nil httpResponse:nil responseObject:nil];
+    return [self initWithRequest:nil httpResponse:nil object:nil error:nil];
 }
 
-- (id)initWithRequest:(MJApiRequest*)request httpResponse:(NSHTTPURLResponse*)response error:(NSError*)error
+- (id)initWithRequest:(MJApiRequest*)request
+         httpResponse:(NSHTTPURLResponse*)response
+               object:(id)responseObject
+                error:(NSError*)error
 {
     self = [super init];
     if (self)
     {
         _error = error;
-        _request = request;
-        _httpResponse = response;
-        _responseObject = nil;
-    }
-    return self;
-}
-
-- (id)initWithRequest:(MJApiRequest*)request httpResponse:(NSHTTPURLResponse*)response responseObject:(id)responseObject
-{
-    self = [super init];
-    if (self)
-    {
-        _error = nil;
         _request = request;
         _httpResponse = response;
         _responseObject = responseObject;
@@ -51,7 +41,58 @@
 
 - (NSString*)description
 {
-    return [NSString stringWithFormat:@"\n\nREQUEST: %@\n\nERROR: %@\n\nHTTP RESPONSE: %@\n\nOBJECT: %@\n\n", _request.description, _error.description, _httpResponse.description, [_responseObject description]];
+    return [NSString stringWithFormat:@"\n\nREQUEST: %@\n\nERROR: %@\n\nHTTP RESPONSE: %@\n\nOBJECT: %@\n\n",
+            _request.description,
+            _error.description,
+            _httpResponse.description,
+            [_responseObject description]
+            ];
+}
+
+#pragma mark - Protocols
+#pragma mark NSCoding
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    [aCoder encodeObject:_error forKey:@"error"];
+    [aCoder encodeObject:_request forKey:@"request"];
+    [aCoder encodeObject:_httpResponse forKey:@"httpResponse"];
+    [aCoder encodeObject:_httpResponse.URL forKey:@"httpResponse.url"];
+    [aCoder encodeInteger:_httpResponse.statusCode forKey:@"httpResponse.statusCode"];
+    [aCoder encodeObject:@"HTTP/1.1" forKey:@"httpResponse.version"];
+    [aCoder encodeObject:_httpResponse.allHeaderFields forKey:@"httpResponse.headerFields"];
+    [aCoder encodeObject:_responseObject forKey:@"responseObject"];
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    if (self)
+    {
+        _error = [aDecoder decodeObjectForKey:@"error"];
+        _request = [aDecoder decodeObjectForKey:@"request"];
+        _httpResponse = [[NSHTTPURLResponse alloc] initWithURL:[aDecoder decodeObjectForKey:@"httpResponse.url"]
+                                                    statusCode:[aDecoder decodeIntegerForKey:@"httpResponse.statusCode"]
+                                                   HTTPVersion:[aDecoder decodeObjectForKey:@"httpResponse.version"]
+                                                  headerFields:[aDecoder decodeObjectForKey:@"httpResponse.headerFields"]];
+        _responseObject = [aDecoder decodeObjectForKey:@"responseObject"];
+    }
+    return self;
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    NSHTTPURLResponse *httpResponse = [[NSHTTPURLResponse alloc] initWithURL:_httpResponse.URL
+                                                                  statusCode:_httpResponse.statusCode
+                                                                 HTTPVersion:@"HTTP/1.1"
+                                                                headerFields:[_httpResponse.allHeaderFields copy]];
+    
+    MJApiResponse *response = [[MJApiResponse allocWithZone:zone] initWithRequest:[_request copy]
+                                                                     httpResponse:httpResponse
+                                                                           object:[_responseObject copy]
+                                                                            error:[_error copy]];
+    
+    return response;
 }
 
 @end
